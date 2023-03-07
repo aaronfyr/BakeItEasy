9ab05aace7bb6eb85f7d049919719b1ae8d64445
micaellaa <99943347+micaellaa@users.noreply.github.com>
2023-03-07 14:22:54 +0800

    Initialised React app
    Created additional folders as well

---
+++ NetBeansProjects/BakeItEasy/BakeItEasy-react/bakeiteasy/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+//import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import BuyerHomepage from "./screens/buyerHomepage";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<BuyerHomepage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ NetBeansProjects/BakeItEasy/BakeItEasy-react/bakeiteasy/src/screens/buyerHomepage.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./homepage.css";
+
+////<FaRecycle size={33} color="green" />
+function BuyerHomepage() {
+  return <div></div>;
+}
+
+export default BuyerHomepage;
