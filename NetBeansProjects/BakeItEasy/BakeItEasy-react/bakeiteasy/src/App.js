676e6ea3d45a4d51fe38a938c19629bb0306c8e8
alantay11 <alantay1111@gmail.com>
2023-03-17 11:51:17 +0800

    shift from screens to pages

--- NetBeansProjects/BakeItEasy/BakeItEasy-react/bakeiteasy/src/App.js
+++ NetBeansProjects/BakeItEasy/BakeItEasy-react/bakeiteasy/src/App.js
@@ -2,14 +2,15 @@ import React from "react";
 //import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import BuyerLogin from "./screens/buyerLogin";
-import SellerLogin from "./screens/sellerLogin";
-import SellerHomepage from "./screens/sellerHomePage";
 import fetchListings from "./api/listings";
 import { ChakraProvider } from "@chakra-ui/react";
 
+import BuyerLogin from "./pages/buyerLogin";
+import SellerLogin from "./pages/sellerLogin";
+import SellerHomepage from "./pages/sellerHomePage";
 import BuyerHomepage from "./pages/buyerHomepage";
 import BuyerListingPage from "./pages/buyerListingPage";
+
 
 function App() {
   return (
