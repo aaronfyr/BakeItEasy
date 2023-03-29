fdada4d3471acb9b8532a6d45cfd73ddb757f211
Aaron <e0694465@u.nus.edu>
2023-03-29 15:13:12 +0800

    Removed some imports

--- NetBeansProjects/BakeItEasy/BakeItEasy-ejb/src/java/ejb/session/stateless/ListingSessionBeanLocal.java
+++ NetBeansProjects/BakeItEasy/BakeItEasy-ejb/src/java/ejb/session/stateless/ListingSessionBeanLocal.java
@@ -30,8 +30,6 @@ public interface ListingSessionBeanLocal {
     public void updateListing(Listing updatedListing) throws InputDataValidationException, ListingNotFoundException;
 
     public void deleteListing(Long listingId) throws ListingNotFoundException, ListingHasOngoingOrdersException, OrderNotFoundException;
-    
-    public List<Order> retrieveOrdersByListingId(Long listingId);
 
     public Listing retrieveListingByListingId(Long listingId) throws ListingNotFoundException;
     
@@ -54,5 +52,7 @@ public interface ListingSessionBeanLocal {
     public List<Listing> retrieveListingByPriceRange(BigDecimal startPrice, BigDecimal endPrice);
 
     public Boolean doesListingHaveOutstandingOrders(Listing listing);
+
+    public List<Order> retrieveOrdersByListingId(Long listingId);
     
 }
